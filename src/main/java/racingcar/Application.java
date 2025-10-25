b1b24dd31cd038ae6e24c7140d143d1b43c001a3
Jay <92563314+junseok-jay@users.noreply.github.com>
2025-10-26 00:44:29 +0900

    feat: 최종 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.controller.gameController;
+import racingcar.service.gameManager;
+import racingcar.view.gameView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        final gameView gv = new gameView();
+        gameController gamecontroller = new gameController(gv, new gameManager(gv));
+        gamecontroller.gameRun();
     }
 }
--- src/main/java/racingcar/controller/gameController.java
+++ src/main/java/racingcar/controller/gameController.java
@@ -1,4 +1,20 @@
 package racingcar.controller;
 
+import racingcar.service.gameManager;
+import racingcar.view.gameView;
+
 public class gameController {
+    private gameView gameview;
+    private gameManager gamemanager;
+
+    public gameController(gameView gameview, gameManager gamemanager){
+        this.gameview = gameview;
+        this.gamemanager = gamemanager;
+    }
+
+    public void gameRun(){
+        gameView.GameInput newGame = gameview.input();
+        var racers = gamemanager.raceRun(newGame.racers(), newGame.trial());
+        gameview.output(gamemanager.whoRacerWin(racers));
+    }
 }
